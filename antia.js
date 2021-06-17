7ec75721c306c55d3386eff0dfa883906ff691b3
VJMC <victor.es@gmail.com>
2021-06-17 12:59:37 +0200

    Add files via upload

---
+++ antia.js
@@ -0,0 +1,217 @@
+function imagesCarousel() {
+	
+	return {
+		active: 0,
+		photos: [
+			{
+				id: 1,
+				src:
+					"./img/1.jpg",
+				alt: "1 DIA/S"
+			},
+			{
+				id: 2,
+				src:
+					"./img/2.jpg",
+				alt: "2 DIA/S"
+			},
+			{
+				id: 3,
+				src:
+					"./img/3.jpg",
+				alt: "3 DIA/S"
+			},
+			{
+				id: 4,
+				src:
+					"./img/3.1.jpg",
+				alt: "3 DIA/S"
+			},
+			{
+				id: 5,
+				src:
+					"./img/3.2.jpg",
+				alt: "3 DIA/S"
+			},
+			{
+				id: 6,
+				src:
+					"./img/4.jpg",
+				alt: "4 DIA/S"
+			},
+			{
+				id: 7,
+				src:
+					"./img/4.1.jpg",
+				alt: "4 DIA/S"
+			},
+			{
+				id: 8,
+				src:
+					"./img/6.jpg",
+				alt: "6 DIA/S"
+			},
+			{
+				id: 9,
+				src:
+					"./img/7.jpg",
+				alt: "7 DIA/S"
+			},
+			{
+				id: 10,
+				src:
+					"./img/7.1.jpg",
+				alt: "7 DIA/S"
+			},
+			{
+				id: 11,
+				src:
+					"./img/8.jpg",
+				alt: "8 DIA/S"
+			},
+			{
+				id: 12,
+				src:
+					"./img/9.jpg",
+				alt: "9 DIA/S"
+			},
+			{
+				id: 13,
+				src:
+					"./img/9.1.jpg",
+				alt: "9 DIA/S"
+			},
+			{
+				id: 14,
+				src:
+					"./img/9.2.jpg",
+				alt: "9 DIA/S"
+			},
+			{
+				id: 15,
+				src:
+					"./img/10.jpg",
+				alt: "10 DIA/S"
+			},
+			{
+				id: 16,
+				src:
+					"./img/12.jpg",
+				alt: "12 DIA/S"
+			},
+			{
+				id: 17,
+				src:
+					"./img/13.jpg",
+				alt: "13 DIA/S"
+			},
+			{
+				id: 18,
+				src:
+					"./img/24.jpg",
+				alt: "24 DIA/S"
+			},
+			{
+				id: 19,
+				src:
+					"./img/27.jpg",
+				alt: "27 DIA/S"
+			},
+			{
+				id: 20,
+				src:
+					"./img/32.jpg",
+				alt: "32 DIA/S"
+			},
+			{
+				id: 21,
+				src:
+					"./img/32.1.jpg",
+				alt: "32 DIA/S"
+			},
+			{
+				id: 22,
+				src:
+					"./img/34.jpg",
+				alt: "34 DIA/S"
+			},
+			{
+				id: 23,
+				src:
+					"./img/36.jpg",
+				alt: "36 DIA/S"
+			}
+
+
+		]
+	};
+}
+
+/*
+			{
+				id: 13,
+				src:
+					"./img/9.jpg",
+				alt: "9 DIA/S"
+			},
+			{
+				id: 14,
+				src:
+					"./img/9.1.jpg",
+				alt: "9 DIA/S"
+			},
+			{
+				id: 15,
+				src:
+					"./img/10.jpg",
+				alt: "10 DIA/S"
+			},
+			{
+				id: 16,
+				src:
+					"./img/12.jpg",
+				alt: "12 DIA/S"
+			},
+			{
+				id: 17,
+				src:
+					"./img/13.jpg",
+				alt: "13 DIA/S"
+			},
+			{
+				id: 18,
+				src:
+					"./img/24.jpg",
+				alt: "24 DIA/S"
+			},
+			{
+				id: 19,
+				src:
+					"./img/27.jpg",
+				alt: "27 DIA/S"
+			},
+			{
+				id: 20,
+				src:
+					"./img/32.jpg",
+				alt: "32 DIA/S"
+			},
+			{
+				id: 21,
+				src:
+					"./img/32.1.jpg",
+				alt: "32 DIA/S"
+			},
+			{
+				id: 22,
+				src:
+					"./img/34.jpg",
+				alt: "34 DIA/S"
+			},
+			{
+				id: 23,
+				src:
+					"./img/36.jpg",
+				alt: "36 DIA/S"
+			}
+*/
